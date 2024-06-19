1578d5b9df1f4ca6e3aba510e4211684ec1d17c2
Beadko <beadko@gmail.com>
2024-06-19 14:57:49 +0700

    Create AddWine popup

--- src/components/WineList.vue
+++ src/components/WineList.vue
@@ -1,5 +1,15 @@
 <template>
     <h1> Your Wine List</h1>
+    <div class="card flex justify-center">
+        <Button label="+ Add Wine" @click="visible = true" />
+        <Dialog v-model:visible="visible" modal header="What wine would you like to add?" :style="{ width: '25rem' }">
+            <AddWine></AddWine>
+            <div class="flex justify-end gap-2">
+                <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+                <Button type="button" label="Save" @click="visible = false"></Button>
+            </div>
+        </Dialog>
+    </div> 
     <DataTable :value="wines" dataKey="id" tableStyle="min-width: 60rem">
         <Column field="name" header="Name"></Column>
         <Column field="wine_type" header="Type"></Column>
@@ -8,11 +18,16 @@
 
 <script>
 import axios from 'axios';
+import AddWine from './AddWine.vue'
 export default {
     name: "WineList",
+    components: {
+        AddWine
+    },
     data() {
         return {            
             wines:[],
+            visible: false
         }
     },
     methods: {
@@ -24,8 +39,8 @@ export default {
             .catch((error) => {
                 window.alert(`The API returned an error: ${error}`);
             })
-        }
-  },
+        },
+    },
     mounted() {
         this.getWines();
     }
--- src/main.js
+++ src/main.js
@@ -4,16 +4,28 @@ import Aura from '@primevue/themes/aura'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
 import Button from 'primevue/button'
+import Select from 'primevue/select'
+import InputText from 'primevue/inputtext'
+import Dialog from 'primevue/dialog'
 import App from './App.vue'
 
 const app = createApp(App);
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: Aura,
+        options: {
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
     }
 });
 app.component('Column', Column)
 app.component('DataTable', DataTable)
 app.component('Button', Button)
+app.component('Select', Select)
+app.component('InputText', InputText)
+app.component('Dialog', Dialog)
 
 app.mount('#app')
