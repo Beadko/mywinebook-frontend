51b231ab065ac4e6e68ed0a63e1f2966b029a76d
Beadko <beadko@gmail.com>
2024-06-27 12:15:24 +0700

    Implement AddWine

--- src/components/AddWine.vue
+++ src/components/AddWine.vue
@@ -4,15 +4,15 @@
         <Dialog v-model:visible="visible" modal header="What wine would you like to add?" :style="{ width: '25rem' }">
             <div class="flex items-center gap-4 mb-4">
             <label for="name" class="font-semibold w-24">Name</label>
-            <InputText id="name" class="flex-auto" autocomplete="off" v-model="wines" />
+            <InputText id="name" class="flex-auto" autocomplete="off" v-model="selected.name" />
         </div>
         <div class="flex items-center gap-4 mb-8">
             <label for="wine_types" class="font-semibold w-24">Type</label>
-            <Select v-model="selected" :options="wine_types" optionLabel="name" optionValue="id" class="w-full md:w-[14rem]" />
+            <Select v-model="selected.wine_type" :options="wine_types" optionLabel="name" optionValue="id" class="w-full md:w-[14rem]" />
         </div>
             <div class="flex justify-end gap-2">
-                <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
-                <Button type="button" label="Save" @click="visible = false; addWine"></Button>
+                <Button type="button" label="Cancel" severity="secondary" @click="visible = false" ></Button>
+                <Button type="button" label="Save" @click="addWine"></Button>
             </div>
         </Dialog>
     </div> 
@@ -27,20 +27,22 @@ export default {
     name: "AddWine",
     data() {
         return {            
-            wines:[],
             visible: false,
-            selected: ref(),
+            selected: ref({
+            name:'',
+            wine_type: ''
+            }),
             wine_types: ref([])
         }
     },
     methods: {
         addWine() {
-            const formData = new FormData()
-            formData.append("name", "wine_type")
-            const { data }= axios.post("http://localhost:8081/wine", formData)
+            this.visible = false
+            axios.post("http://localhost:8081/wine", this.selected)
             .then(res => {
-                this.wines =res.data;
-            })
+                console.log(res.data)  
+                window.alert(`Yay! Another wine added to your library`);          
+             })
             .catch((error) => {
                 window.alert(`The API returned an error: ${error}`);
             })
@@ -53,10 +55,7 @@ export default {
             .catch((error) => {
                 window.alert(`The API returned an error: ${error}`);
             })
-        },
-    },
-    beforeMount() {
-        this.addWine()
+        }
     },
     mounted() {
         this.getWineTypes();
